89b4b5d8ba6fd9cc6f1f22921b457273ce52cfea
lolluslollus <lolluslollus@users.noreply.github.com>
2022-10-01 21:59:06 +0200

    texts

--- strings.lua
+++ strings.lua
@@ -1,8 +1,8 @@
 function data()
 	return {
 		en = {
-			["ModDesc"] = "Plop streetside passenger stations as usual, they will have extra capacity.",
-			["ModName"] = "Extra capacity for streetside passenger stations",
+			["ModDesc"] = "Plop streetside bus or tram stops as usual, they will have extra capacity.",
+			["ModName"] = "Extra capacity for streetside bus or tram stops",
 		},
 	}
 end

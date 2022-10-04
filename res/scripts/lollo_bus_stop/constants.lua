6b37faa61952c2cf961c300d781c21323b7f614f
lolluslollus <lolluslollus@users.noreply.github.com>
2022-10-05 00:38:03 +0200

    more comments and tests

--- res/scripts/lollo_bus_stop/constants.lua
+++ res/scripts/lollo_bus_stop/constants.lua
@@ -1,7 +1,7 @@
 return {
     cargoNodeZ = -2,
     halfEdgeLength = 0.5,
-    outerEdgeX = 1, -- 1 is enough for straightaways, curves need more than 5. The longer this is, the more buildings will be destroyed.
+    outerEdgeX = 1, -- 1 is enough for straightaways, curves need more than 10. The longer this is, the more buildings will be destroyed; 10 is a killer already.
     -- outerEdgeXCon = 0.7, -- we make this smaller then outerEdgeX so we can snap in curves -- useless, curves fail anyway
     outerEdgeXCon = 1,
     vehicleNodeZ = 0

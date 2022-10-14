17ea023b40d5f6335f7e78adea24200eeb6b0008
lolluslollus <lolluslollus@users.noreply.github.com>
2022-10-14 12:46:59 +0200

    add savegame to repro crash

---
+++ reproduce-crash/savegame/lollo-test-02-dump.sav.lua
@@ -0,0 +1,33 @@
+function data()
+return {
+	["arrivaltracker.lua"] = {
+		data = {
+			__ug_gui_game_info_cargo = 0,
+			__ug_gui_game_info_passenger = 0,
+		},
+		filterdata = {
+			__ug_gui_game_info_cargo = { },
+			__ug_gui_game_info_passenger = {
+				cargotype = "PASSENGERS",
+			},
+		},
+	},
+	["contexthelper.lua"] = {
+		openAtStart = false,
+	},
+	["gameinfo.lua"] = { },
+	["guidesystem.lua"] = {
+		active = true,
+		completedGuides = {
+			buildBusStop = true,
+		},
+		time = 259543.60000612,
+		timeLastGuide = -9999,
+		timeLastProposal = 259543.40000612,
+	},
+	["lollo_bus_stop.lua"] = {
+		isWorking = false,
+	},
+	["selectortooltip.lua"] = { },
+}
+end
